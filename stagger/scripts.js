01433fe44d53d6ec705a2b372a4019b29c8fa6be
Piyush Verma <piyus15101999@gmail.com>
2021-05-22 00:20:06 +0530

    added a ripple effect

--- stagger/scripts.js
+++ stagger/scripts.js
@@ -1,7 +1,7 @@
 var container=document.querySelector(".container")
 console.log(container);
 var temp;
-for(let i=0;i<81;i++){
+for(let i=0;i<225;i++){
     temp = document.createElement('div')
     temp.classList.add('particle')
     container.appendChild(temp)
@@ -10,15 +10,19 @@ const tl=anime.timeline({loop:true})
 tl.add({
     targets:".particle",
     scale:0.1,
-    delay:anime.stagger(100,{grid:[9,9],from:0}),
-    duration:500,
+    delay:anime.stagger(50,{grid:[15,15],from:0}),
+    duration:600,
+    translateX:200,
+    translateY:200,
     easing:"easeInOutQuad",
     backgroundColor:"#fff"
 }).add({
     targets:".particle",
     scale:1,
-    delay:anime.stagger(100,{grid:[9,9],from:0}),
-    duration:500,
+    delay:anime.stagger(50,{grid:[15,15],from:0}),
+    duration:600,
+    translateX:0,
+    translateY:0,
     easing:"easeInOutQuad",
-    backgroundColor:"#dc143c"
-},"-=150")
+    backgroundColor:"#444"
+},"-=10")
